85987d90876b12a321f181a22fe59181fce28797
oscarbagger <oscarbagger@live.dk>
2020-03-01 21:18:31 +0100

    images

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
-document.addEventListener("DOMContentLoaded", GetJson);
+document.addEventListener("DOMContentLoaded", start);
 
-const template=document.querySelector("template");
-const single=document.querySelector("#singleView");
-const main=document.querySelector("#studentList");
+const template = document.querySelector("template");
+const single = document.querySelector("#singleView");
+const main = document.querySelector("#studentList");
 var studentList;
 
-var singleOpen=false;
+var singleOpen = false;
 
-async function GetJson()
-{
-    const jsonData = await fetch("https://oscarbagger.com/kea/studentlist/students1991.json");
-    studentList=await jsonData.json();
-    console.log(studentList);
-    ShowList();
-    single.addEventListener("click", ()=> {
-        if (singleOpen) {
-            single.style.display="none";
-            singleOpen=false;
-        }
-    })
+const settings = {
+  filter: null,
+  sortBy: null,
+  sortDir: "asc"
+};
+
+// student prototype
+const Student = {
+  fullName: "",
+  gender: "",
+  house: ""
+};
+
+function start() {
+  loadJson();
 }
 
-function ShowList()
-{
-    studentList.forEach(student => {
-        console.log(student.fullname);
-        let clone=template.cloneNode(true).content;
-        
-        clone.querySelector(".name").textContent=student.fullname;
-        clone.querySelector(".house").textContent=student.house;
-        
-        main.appendChild(clone);
-        main.lastElementChild.addEventListener("click",() => {
-            // show more info
-            if(singleOpen==false)  {
-                single.style.display="block";
-                singleOpen=true;
-                single.querySelector(".name").textContent=student.fullname;
-                single.querySelector(".house").textContent=student.house;
-                }
-        });
-    })
+async function loadJson() {
+  const jsonData = await fetch(
+    "https://petlatkea.dk/2020/hogwarts/students.json "
+  );
+  studentList = await jsonData.json();
+
+  ShowList();
+
+  single.addEventListener("click", () => {
+    if (singleOpen) {
+      single.style.display = "none";
+      singleOpen = false;
+    }
+  });
+}
+
+function PrepareObjects() {}
+
+function ShowList() {
+  studentList.forEach(student => {
+    console.log(student.fullname);
+    let clone = template.cloneNode(true).content;
+
+    clone.querySelector(".name").textContent = student.fullname;
+    clone.querySelector(".house").textContent = student.house;
+
+    main.appendChild(clone);
+    main.lastElementChild.addEventListener("click", () => {
+      // show more info
+      if (singleOpen == false) {
+        single.style.display = "block";
+        singleOpen = true;
+        single.querySelector(".name").textContent = student.fullname;
+        single.querySelector(".house").textContent = student.house;
+      }
+    });
+  });
 }
